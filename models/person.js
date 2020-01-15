5b56cfacdc7c1d4118a9c9cfa966a8494cc92fd9
Kieran-C <kieran.caruana@gmail.com>
2020-01-15 12:20:11 +0000

    Completed Task 2

--- models/person.js
+++ models/person.js
@@ -1,1 +1,19 @@
 // Your Person class goes here!
+class Person {
+  constructor(firstName, surname, dob){
+    this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1)
+    this.surname = surname
+    this.dob = dob
+    this.emails = []
+  }
+
+  fullName(){
+    return this.firstName + ' ' + this.surname
+  }
+
+  addEmail(email){
+    this.emails.push(email)
+  }
+}
+
+module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -2,3 +2,34 @@ const chai = require('chai')
 const expect = chai.expect
 
 // Your Person test goes here!
+
+const Person = require('../models/person.js')
+
+describe('Person', ()=> {
+  it('should initialize properly', ()=> {
+    var person = new Person('Joe', 'Bloggs', '1 Jan 1998')
+    expect(person.firstName).to.equal('Joe')
+    expect(person.surname).to.equal('Bloggs')
+    expect(person.dob).to.equal('1 Jan 1998')
+
+    expect(person.emails).to.deep.equal([])
+  })
+
+  it('should capatalize firstName', ()=> {
+    var person = new Person('amy', 'Nother', '1 Jan 1998')
+    expect(person.firstName).to.equal('Amy')
+  })
+
+  it('should return a full name', ()=> {
+    var person = new Person('amy', 'Nother', '1 Jan 1998')
+    expect(person.fullName()).to.equal('Amy Nother')
+  })
+
+  it('should accept new emails and add them to the array', ()=> {
+    var person = new Person('Joe', 'Bloggs', '1 Jan 1998')
+
+    person.addEmail('joe@example.com')
+    person.addEmail('joe.bloggs@workexample.com')
+    expect(person.emails).to.deep.equal(['joe@example.com', 'joe.bloggs@workexample.com'])
+  })
+})
